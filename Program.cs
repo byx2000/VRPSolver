fc4b1eea3e4d9faf9101befc9b0125ea98720c04
byx <byx@DESKTOP-CMOEPQG>
2020-01-15 09:32:48 +0800

    添加使用示例

--- Program.cs
+++ Program.cs
@@ -1,40 +1,8 @@
-﻿using System;
-
-class Program
-{
-    // 当求解完成时，此方法将会被调用
-    static private void OnFinish(int[][] path, double[] load, double[] mileage)
-    {
-        Console.WriteLine("求解完成！\n");
-
-        for (int i = 0; i < path.Length; ++i)
-        {
-            Console.Write("Car " + i + ":\t");
-            for (int j = 0; j < path[i].Length; ++j)
-            {
-                Console.Write(path[i][j] + " ");
-            }
-
-            Console.WriteLine();
-            Console.Write("load: " + load[i] + " ");
-            Console.Write("mileage: " + mileage[i]);
-            Console.WriteLine();
-            Console.WriteLine();
-        }
-    }
+﻿class Program
+{ 
     static void Main()
     {
-        // VRP问题相关数据
-        double[] x = { 12.32, 5.96, 7.73, 10.57, 18.27, 17.55, 18.95, 19.39, 12.75, 11.7, 8.45 };
-        double[] y = { 8.35, 9.39, 7.07, 3.55, 3.97, 6.72, 11.01, 14.33, 14.73, 10.6, 13.48 };
-        double[] demand = { 0.0, 1.4, 1.5, 0.3, 0.5, 1.7, 0.1, 1.3, 1.1, 2.5, 2.2 };
-        double[] capacity = { 5.0, 5.0, 5.0, 5.0, 5.0 };
-        double[] disLimit = { 20.0, 20.0, 20.0, 20.0, 20.0 };
-        double k1 = 100.0;
-        double k2 = 1.0;
-        double k3 = 1.0;
-
-        // 求解
-        VRPSolver.Solve(x, y, demand, capacity, disLimit, k1, k2, k3, OnFinish);
+        Sample1.Run();
+        Sample2.Run();
     }
 }
---
+++ Sample2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+
+class Sample2
+{
+    // 当求解完成时，此方法将会被调用
+    static private void OnFinish(int[][] path, double[] load, double[] mileage)
+    {
+        Console.WriteLine("Sample2求解完成！\n");
+
+        double maxMileage = 0.0;
+        for (int i = 0; i < path.Length; ++i)
+        {
+            if (path[i].Length == 0) continue;
+
+            Console.Write("Car " + i + ": 0->");
+            for (int j = 0; j < path[i].Length; ++j)
+            {
+                Console.Write(path[i][j] + "->");
+            }
+            Console.WriteLine("0");
+
+            Console.WriteLine("载重：" + load[i] + " 里程：" + mileage[i]);
+            Console.WriteLine();
+
+            maxMileage = Math.Max(maxMileage, mileage[i]);
+        }
+
+        Console.WriteLine("时间：" + maxMileage);
+    }
+
+    static public void Run()
+    {
+        // 50个配送点   文件输入
+
+        // 打开文件
+        StreamReader reader = new StreamReader("in.txt");
+        string txt = reader.ReadToEnd();
+        char[] sep = { ' ', '\t', '\n' };
+        string[] input = txt.Split(sep);
+
+        int index = 0;
+
+        // 解析数据
+        int numNode = Convert.ToInt32(input[index++]);
+        double[] x = new double[numNode];
+        double[] y = new double[numNode];
+        double[] demand = new double[numNode];
+        for (int i = 0; i < numNode; ++i)
+        {
+            x[i] = Convert.ToDouble(input[index++]);
+            y[i] = Convert.ToDouble(input[index++]);
+            demand[i] = Convert.ToDouble(input[index++]);
+        }
+
+        int numCar = Convert.ToInt32(input[index++]);
+        double[] capacity = new double[numCar];
+        double[] disLimit = new double[numCar];
+        for (int i = 0; i < numCar; ++i)
+        {
+            capacity[i] = Convert.ToDouble(input[index++]);
+            disLimit[i] = Convert.ToDouble(input[index++]);
+        }
+
+        double k1 = Convert.ToDouble(input[index++]);
+        double k2 = Convert.ToDouble(input[index++]);
+        double k3 = Convert.ToDouble(input[index++]);
+
+        // 求解
+        VRPSolver.Solve(x, y, demand, capacity, disLimit, k1, k2, k3, OnFinish);
+    }
+}
